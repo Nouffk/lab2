a57ffc666960bc8aab98f2638e47b6666aac45d3
Nouf <nalknedri@stu.kau.edu.sa>
2024-04-19 11:43:21 +0300

    Initial Commit

---
+++ lab2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab2
+//
+//  Created by Nouf Faisal  on 10/10/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab2/lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  lab2App.swift
+//  lab2
+//
+//  Created by Nouf Faisal  on 10/10/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
